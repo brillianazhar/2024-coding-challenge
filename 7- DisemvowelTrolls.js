13c5ecf80e7e3f6fba60fa912e1e546e5c093284
brillianazhar <brilianazhardanet@gmail.com>
2024-02-04 12:17:36 +0700

    Day 7

---
+++ 7- DisemvowelTrolls.js
@@ -0,0 +1,6 @@
+// Codewars Coding Challenge 2024
+// https://www.codewars.com/kata/52fba66badcd10859f00097e/javascript
+
+const disemvowel = (str) => str.replace(/[aeiou]/gi, '');
+
+console.log(disemvowel("This website is for losers LOL!"));
