9fe65b443cc96212d26804469323ed88f378f567
brillianazhar <brilianazhardanet@gmail.com>
2024-02-18 10:17:03 +0700

    Day 20

---
+++ 20-FriendOrFoe.js
@@ -0,0 +1,8 @@
+// Codewars Coding Challenge 2024
+// https://www.codewars.com/kata/55b42574ff091733d900002f
+
+function friend(friends) {
+  return friends.filter((a) => a.length === 4);
+}
+
+console.log(friend(["Ryan", "Kieran", "Mark"]));
