9cb72b46229a09816b8d0a442f2dd597f813d665
brillianazhar <brilianazhardanet@gmail.com>
2024-02-21 13:27:16 +0700

    Day 23

---
+++ 23-NextPerfectSquare.js
@@ -0,0 +1,9 @@
+// Codewars Coding Challenge 2024
+// https://www.codewars.com/kata/56269eb78ad2e4ced1000013
+
+function findNextSquare(sq) {
+  // Return the next square if sq is a perfect square, -1 otherwise
+  return -1;
+}
+
+console.log(findNextSquare(121));
