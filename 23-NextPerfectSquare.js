c01e2e047fd138491dbb5aa531c3088805860d75
brillianazhar <brilianazhardanet@gmail.com>
2024-02-21 13:28:08 +0700

    Day 23

--- 23-NextPerfectSquare.js
+++ 23-NextPerfectSquare.js
@@ -2,8 +2,7 @@
 // https://www.codewars.com/kata/56269eb78ad2e4ced1000013
 
 function findNextSquare(sq) {
-  // Return the next square if sq is a perfect square, -1 otherwise
-  return -1;
+  return Math.sqrt(sq) % 1 === 0 ? (Math.sqrt(sq) + 1) ** 2 : -1;
 }
 
-console.log(findNextSquare(121));
+console.log(findNextSquare(625));
