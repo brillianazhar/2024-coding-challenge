d4756906d73a709f020e7bde4858c5e0dbe5a6b4
brillianazhar <brilianazhardanet@gmail.com>
2024-02-22 19:54:18 +0700

    Day 24: fix better code

--- 24-PrinterError.js
+++ 24-PrinterError.js
@@ -2,9 +2,7 @@
 // https://www.codewars.com/kata/56541980fa08ab47a0000040
 
 function printerError(s) {
-  return `${s.match(/[n-z]/gi) === null ? 0 : s.match(/[n-z]/gi).length}/${
-    s.length
-  }`;
+  return `${(s.match(/[n-z]/gi) || []).length}/${s.length}`;
 }
 
 console.log(printerError("aaaxbbbbyyhwawiwjjjwwm"));
