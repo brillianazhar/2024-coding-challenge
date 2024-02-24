6e3459a647e75c5f1ca0ce8642e9ba17e62489ce
brillianazhar <brilianazhardanet@gmail.com>
2024-02-24 13:13:25 +0700

    Day 26

---
+++ 26-Binary.js
@@ -0,0 +1,8 @@
+// Codewars Coding Challenge 2024
+// https://www.codewars.com/kata/551f37452ff852b7bd000139
+
+function addBinary(a, b) {
+  return (a + b).toString(2);
+}
+
+console.log(addBinary(5, 9));
