4eaefcb3efba114b490db5be24e49470ed41be06
brillianazhar <brilianazhardanet@gmail.com>
2024-03-05 17:02:08 +0700

    Day 36

---
+++ 36-HighestProfit.js
@@ -0,0 +1,8 @@
+// Codewars Coding Challenge 2024
+// https://www.codewars.com/kata/559590633066759614000063
+
+function minMax(arr) {
+  return [Math.min(...arr), Math.max(...arr)];
+}
+
+console.log(minMax([1, 2, 3, 4, 5]));
