f766eeaece8969066323d9caab3fecf02fd17029
brillianazhar <brilianazhardanet@gmail.com>
2024-03-09 12:28:08 +0700

    Day 40

---
+++ 40-ChocolateProblem.js
@@ -0,0 +1,8 @@
+// Codewars Coding Challenge 2024
+// https://www.codewars.com/kata/534ea96ebb17181947000ada
+
+function breakChocolate(n, m) {
+  return n * m <= 1 ? 0 : n * m - 1;
+}
+
+console.log(breakChocolate(5, 5));
