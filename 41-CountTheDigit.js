ce1e02efec429a01b1e0e5da2e3a8da10dd0b1d1
brillianazhar <brilianazhardanet@gmail.com>
2024-03-10 19:22:00 +0700

    Day 41

--- 41-CountTheDigit.js
+++ 41-CountTheDigit.js
@@ -2,18 +2,24 @@
 // https://www.codewars.com/kata/566fc12495810954b1000030
 
 function nbDig(n, d) {
-  let result = 0;
-  Array.from({ length: n + 1 }, (_, i) => i * i)
-    .filter((num) => String(num).includes(`${d}`))
-    .map((nums) => {
-      let a = nums.toString();
-      for (let i = 0; i < a.length; i++) {
-        if (a[i] === d.toString()) {
-          result++;
-        }
-      }
-    });
-  return result;
+  // let result = 0;
+  // Array.from({ length: n + 1 }, (_, i) => i * i)
+  //   .filter((num) => String(num).includes(`${d}`))
+  //   .map((nums) => {
+  //     let a = nums.toString();
+  //     for (let i = 0; i < a.length; i++) {
+  //       if (a[i] === d.toString()) {
+  //         result++;
+  //       }
+  //     }
+  //   });
+  // return result;
+
+  //Cara lain
+  return Array.from({ length: n + 1 }, (_, i) => i * i)
+    .toString()
+    .split("")
+    .filter((num) => num === `${d}`).length;
 }
 
-console.log(nbDig(5750, 0));
+console.log(nbDig(25, 1));
