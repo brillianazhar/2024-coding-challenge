d999800bd038860ee41d0e094f3419b5e0b17ec0
brillianazhar <brilianazhardanet@gmail.com>
2024-03-10 19:13:47 +0700

    Day 41

---
+++ 41-CountTheDigit.js
@@ -0,0 +1,19 @@
+// Codewars Coding Challenge 2024
+// https://www.codewars.com/kata/566fc12495810954b1000030
+
+function nbDig(n, d) {
+  let result = 0;
+  Array.from({ length: n + 1 }, (_, i) => i * i)
+    .filter((num) => String(num).includes(`${d}`))
+    .map((nums) => {
+      let a = nums.toString();
+      for (let i = 0; i < a.length; i++) {
+        if (a[i] === d.toString()) {
+          result++;
+        }
+      }
+    });
+  return result;
+}
+
+console.log(nbDig(5750, 0));
