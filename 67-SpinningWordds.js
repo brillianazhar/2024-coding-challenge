af48c351065e1cac1c1f32d957f5a67dfdf98d8c
brillianazhar <brilianazhardanet@gmail.com>
2024-04-07 13:09:36 +0700

    Dy 67

---
+++ 67-SpinningWordds.js
@@ -0,0 +1,11 @@
+// Codewars Coding Challenge 2024
+// https://www.codewars.com/kata/5264d2b162488dc400000001
+
+function spinWords(string) {
+  return string
+    .split(" ")
+    .map((str) => (str.length >= 5 ? str.split("").reverse().join("") : str))
+    .join(" ");
+}
+
+console.log({ "": spinWords("This is another test") });
