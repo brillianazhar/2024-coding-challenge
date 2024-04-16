d1bd55214424618d3097684daece57e00f5f2db0
brillianazhar <brilianazhardanet@gmail.com>
2024-04-16 09:21:30 +0700

    Day 5: fix

--- 5-ReversedString.js
+++ 5-ReversedString.js
@@ -1,15 +1,15 @@
 // Codewars Coding Challenge 2024
 // https://www.codewars.com/kata/5168bb5dfe9a00b126000018/javascript
 
-function solution(str){
-    const abc = [];
-    for (const iterator of str) {
-        abc.push(iterator);
-    }
-    return abc.reverse().join('');
+function solution(str) {
+  const abc = [];
+  for (const iterator of str) {
+    abc.push(iterator);
+  }
+  return abc.reverse().join("");
 }
 
 //Cara Lain
 // const solution = (str) => a = str.split("").reverse().join("");
 
-console.log(solution("Asbahbas"));
+console.log(solution("Asbahbasas"));
